f4261525b980422ce3ba0eee478db5e6152aee10
autotaker <autotaker@gmail.com>
2021-01-06 18:37:29 +0900

    add Test.Method module

---
+++ src/Test/Method.hs
@@ -0,0 +1,12 @@
+{-# OPTIONS_GHC -Wno-missing-import-lists #-}
+
+module Test.Method
+  ( module Test.Method.Matcher,
+    module Test.Method.Mock,
+    module Test.Method.Monitor,
+  )
+where
+
+import Test.Method.Matcher
+import Test.Method.Mock
+import Test.Method.Monitor
