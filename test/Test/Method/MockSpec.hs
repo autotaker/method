56b66b875429f8f69f606e35b7cc4ece9d61b277
autotaker <autotaker@gmail.com>
2021-01-10 10:14:52 +0900

    refs #1 remove MonadThrow constraint

--- test/Test/Method/MockSpec.hs
+++ test/Test/Method/MockSpec.hs
@@ -1,8 +1,8 @@
 module Test.Method.MockSpec where
 
-import Test.Hspec (Selector, Spec, describe, it, shouldReturn, shouldThrow)
+import Test.Hspec (Spec, anyErrorCall, describe, it, shouldReturn, shouldThrow)
 import Test.Method.Matcher (ArgsMatcher (args), args', when)
-import Test.Method.Mock (NoStubException, mockup, thenReturn)
+import Test.Method.Mock (mockup, thenReturn)
 
 spec :: Spec
 spec = do
@@ -14,11 +14,8 @@ spec = do
     it "mock method 1 2 returns 3" $ do
       method 1 2 `shouldReturn` 3
 
-    it "mock method 2 2 throws Exception" $ do
-      method 0 2 `shouldThrow` anyNoStubException
+    it "mock method 2 2 throws ErrorCall" $ do
+      method 0 2 `shouldThrow` anyErrorCall
 
     it "mock method 2 3 returns 42" $ do
       method 2 3 `shouldReturn` 42
-
-anyNoStubException :: Selector NoStubException
-anyNoStubException _ = True
